34b271ba8eda4fa0de5f580a6fd4828427833a71
Emmie Maeda <emmie.maeda@gmail.com>
2025-09-06 18:23:35 -0400

    Add stub user script.

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 This userscript is intended for Wikidot site administrators to back up data related to a site's settings, ban data, and membership.
 
-You can [install the script] here.
+You can [install the script](https://github.com/scpwiki/admin-backup-script/raw/main/admin-backup.user.js) here.
 
 Available under the MIT License.
---
+++ admin-backup.user.js
@@ -0,0 +1,24 @@
+/*
+ * Wikidot admin panel backup userscript
+ *
+ * For installation instructions, see https://05command.wikidot.com/user-scripts
+ *
+ * Contact: https://www.wikidot.com/account/messages#/new/4598089
+ */
+
+// ==UserScript==
+// @name        Wikidot additional user info script
+// @description Adds additional user information on user information
+// @version     v0.3.1
+// @updateURL   https://github.com/scpwiki/admin-backup-script/raw/main/admin-backup.user.js
+// @downloadURL https://github.com/scpwiki/admin-backup-script/raw/main/admin-backup.user.js
+// @include     http://*.wikidot.com/_admin
+// @include     https://*.wikidot.com/_admin
+// ==/UserScript==
+
+
+function main() {
+  // TODO
+}
+
+main();
