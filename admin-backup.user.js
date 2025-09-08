12cfa5cc7473d64d2e98650af988f8e557e7a2b1
Emmie Maeda <emmie.maeda@gmail.com>
2025-09-07 23:03:14 -0400

    Bump userscript version.

--- admin-backup.user.js
+++ admin-backup.user.js
@@ -9,7 +9,7 @@
 // ==UserScript==
 // @name        Wikidot admin panel backup script
 // @description Backs up information from the admin panel of a Wikidot site
-// @version     v0.0.6
+// @version     v0.0.7
 // @updateURL   https://github.com/scpwiki/admin-backup-script/raw/main/admin-backup.user.js
 // @downloadURL https://github.com/scpwiki/admin-backup-script/raw/main/admin-backup.user.js
 // @include     http://*.wikidot.com/_admin
